69ea75656aeba2e273cf89207383fb49c67400c5
FriyadDev <riyadhasanblue2323@gmail.com>
2024-08-15 02:20:45 +0600

    feat: functionality added to merge the video & audio

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,77 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { useState, useRef, useEffect } from "react";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function MergeVideo() {
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const videoRef = useRef(null);
+  const messageRef = useRef(null);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const transcode = async () => {
+    const ffmpeg = ffmpegRef.current;
+    const videoUrl =
+      "https://video.xx.fbcdn.net/v/t39.25447-2/454829554_1243044910384882_990000744712823401_n.mp4?_nc_cat=110&ccb=1-7&_nc_sid=9a5d50&efg=eyJ2ZW5jb2RlX3RhZyI6ImRhc2hfcjJldmV2cDktcjFnZW4ydnA5X3E5MCIsInZpZGVvX2lkIjo3NzE4ODAyMTc4MjI5NjkwfQ%3D%3D&_nc_ohc=OHukotW-BVsQ7kNvgH1mSkI&_nc_ht=scontent.fcgp36-1.fna&oh=00_AYCyuWbLhiWwrDM0EVvOdbL4NMmIv18l0o8i3kiE6UTr1w&oe=66C2C80D";
+    const audioUrl =
+      "https://video.xx.fbcdn.net/v/t39.25447-2/454723110_374052069068953_7717764902501555151_n.mp4?_nc_cat=101&ccb=1-7&_nc_sid=9a5d50&efg=eyJ2ZW5jb2RlX3RhZyI6ImRhc2hfYXVkaW9fYWFjcF82NF9mbm9ybTE0X2ZyYWdfMl9hdWRpbyIsInZpZGVvX2lkIjo3NzE4ODAyMTc4MjI5NjkwfQ%3D%3D&_nc_ohc=otMSY_jqXTsQ7kNvgGRxkmH&_nc_ht=scontent.fcgp36-1.fna&oh=00_AYBUFwpyrOemE1Z85sA7AjWklteuiLtiY4349EoZL8veIQ&oe=66C2BDAA";
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    await ffmpeg.writeFile("video.mp4", await fetchFile(videoUrl));
+    await ffmpeg.writeFile("audio.mp4", await fetchFile(audioUrl));
+    await ffmpeg.exec([
+      "-i",
+      "video.mp4",
+      "-i",
+      "audio.mp4",
+      "-c",
+      "copy",
+      "output.mp4",
+    ]);
+    const data = await ffmpeg.readFile("output.mp4");
+    videoRef.current.src = URL.createObjectURL(
+      new Blob([data.buffer], { type: "video/mp4" })
+    );
+  };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  useEffect(() => {
+    // const load = async () => {
+    //   const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/umd";
+    //   const ffmpeg = ffmpegRef.current;
+    //   ffmpeg.on("log", ({ message }) => {
+    //     messageRef.current.innerHTML = message;
+    //     console.log(message);
+    //   });
+    //   // toBlobURL is used to bypass CORS issue, urls with the same
+    //   // domain can be used directly.
+    //   await ffmpeg.load({
+    //     coreURL: await toBlobURL(
+    //       `${baseURL}/ffmpeg-core.js`,
+    //       "text/javascript"
+    //     ),
+    //     wasmURL: await toBlobURL(
+    //       `${baseURL}/ffmpeg-core.wasm`,
+    //       "application/wasm"
+    //     ),
+    //     workerURL: await toBlobURL(
+    //       `${baseURL}/ffmpeg-core.worker.js`,
+    //       "text/javascript"
+    //     ),
+    //   });
+    //   setLoaded(true);
+    // };
+    // load();
+  }, []);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return loaded ? (
+    <button>
+      <video ref={videoRef} controls></video>
+      <br />
+      <button onClick={transcode}>Transcode webm to mp4</button>
+      <p ref={messageRef}></p>
+      <p>Open Developer Tools (Ctrl+Shift+I) to View Logs</p>
+    </button>
+  ) : (
+    <button>Load ffmpeg-core (~31 MB)</button>
   );
 }
